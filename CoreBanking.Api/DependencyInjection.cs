1907637d512ea85c5fbde72104e085d0bee3e093
adisaomoabegunde <oluwatobilobaadedapo81@gmail.com>
2026-02-16 12:36:35 +0100

    first commit

---
+++ CoreBanking.Api/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using CoreBanking.Application;
+using CoreBanking.Infrastructure;
+
+namespace CoreBanking.Api
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddAppDI(this IServiceCollection services)
+        {
+
+            services.AddApplicationDI()
+                .AddInfrastructureDI();
+
+            return services;
+        }
+    }
+}
