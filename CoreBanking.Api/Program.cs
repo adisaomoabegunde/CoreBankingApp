b19f56b28997ed1eb02f09fecb9acfbca2789e4d
adisaomoabegunde <oluwatobilobaadedapo81@gmail.com>
2026-02-16 13:40:17 +0100

    develop branch

--- CoreBanking.Api/Program.cs
+++ CoreBanking.Api/Program.cs
@@ -12,6 +12,7 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddAppDI();
 
 var app = builder.Build();
+//program.cs file
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
