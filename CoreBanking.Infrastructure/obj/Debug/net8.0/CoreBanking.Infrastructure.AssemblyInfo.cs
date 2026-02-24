9e9e129cc0d912e1f41bc47e119e08b82c4e91e8
adisaomoabegunde <oluwatobilobaadedapo81@gmail.com>
2026-02-24 15:29:24 +0100

    Validation updatedd

--- CoreBanking.Infrastructure/obj/Debug/net8.0/CoreBanking.Infrastructure.AssemblyInfo.cs
+++ CoreBanking.Infrastructure/obj/Debug/net8.0/CoreBanking.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CoreBanking.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00cbf18d8b2ab12007638583340cece6a9f94313")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f41b3dc61ab04aaea40f2d121b081f95f9525576")]
 [assembly: System.Reflection.AssemblyProductAttribute("CoreBanking.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CoreBanking.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
