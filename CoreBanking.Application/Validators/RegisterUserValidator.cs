22b1f06c7aed79649c106e94a84448e4fdaf6e87
adisaomoabegunde <oluwatobilobaadedapo81@gmail.com>
2026-02-24 14:21:34 +0100

    Validation updated

--- CoreBanking.Application/Validators/RegisterUserValidator.cs
+++ CoreBanking.Application/Validators/RegisterUserValidator.cs
@@ -13,14 +13,32 @@ namespace CoreBanking.Application.Validators
         public RegisterUserValidator()
         {
             RuleFor(x => x.Username)
-               .NotEmpty()
-               .MinimumLength(3);
-            RuleFor (x => x.Email)
-                .NotEmpty()
-               .EmailAddress();
+               .NotEmpty().WithMessage("Username is required")
+               .MinimumLength(3).WithMessage("Username must be at least 3 characters.")
+               .MaximumLength(30).WithMessage("Username must not exceed 30 characters.")
+               .Must(username => username != "string")
+               .WithMessage("Username cannot be default value")
+               .Matches("^[a-zA-Z0-9_]+$")
+               .WithMessage("Username can only contain letters, numbers and underscores.");
+
+            RuleFor(x => x.Email)
+                .NotEmpty().WithMessage("Email is required.")
+               .EmailAddress().WithMessage("Invalid email format")
+               .Must(email => email != "string")
+               .WithMessage("Email cannot be default value")
+               .MaximumLength(100);
+
             RuleFor(x => x.Password)
-                .NotEmpty()
-                .MinimumLength(6);
+                .NotEmpty().WithMessage("Password is required.")
+                .MinimumLength(6).WithMessage("Password must be atleast 6 characters.")
+                .MaximumLength(64).WithMessage("Password must not exceed 64 characters.")
+               .Must(password => password != "string")
+               .WithMessage("Password cannot be default value")
+               .Matches(@"[A-Z]").WithMessage("Password must contain at least one uppercase letter")
+               .Matches(@"[a-z]").WithMessage("Password must contain at least one lowercase letter")
+               .Matches(@"[0-9]").WithMessage("Password must contain at least one number")
+               .Matches(@"[\W_]").WithMessage("Password must contain at least one special character");
+
         }
     }
 }
